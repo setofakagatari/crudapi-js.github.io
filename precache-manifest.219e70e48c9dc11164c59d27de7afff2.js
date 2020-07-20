93635dcf590d52312f36a7b1ed6ec64987032d84
setofakagatari <si.camilo2001@gmail.com>
2020-07-20 16:37:53 -0500

    Updates

---
+++ precache-manifest.219e70e48c9dc11164c59d27de7afff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff9862a78dc322affe63d5f208813e2",
+    "url": "/crudapi-js.github.io/index.html"
+  },
+  {
+    "revision": "35e8613d9f890d95776a",
+    "url": "/crudapi-js.github.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cb7616510a6b3eecf786",
+    "url": "/crudapi-js.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35e8613d9f890d95776a",
+    "url": "/crudapi-js.github.io/static/js/2.55f572f7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/crudapi-js.github.io/static/js/2.55f572f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7616510a6b3eecf786",
+    "url": "/crudapi-js.github.io/static/js/main.632c5abd.chunk.js"
+  },
+  {
+    "revision": "ffb52a37e35c6eb6649d",
+    "url": "/crudapi-js.github.io/static/js/runtime-main.ce96db21.js"
+  }
+]);
